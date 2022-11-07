ee33860300e2176e856cb967b87b574c9106a219
Carlo Santos <rcarlosantos89@gmail.com>
2022-11-07 10:08:15 -0600

    clean up relationship migration

--- db/migrate/20221104211914_add_habtm_for_users_and_accounts.rb
+++ db/migrate/20221104211914_add_habtm_for_users_and_accounts.rb
@@ -1,8 +1,8 @@
 class AddHabtmForUsersAndAccounts < ActiveRecord::Migration[6.1]
   def change
     create_table :accounts_users, id: false do |t|
-      t.belongs_to :user, type: :uuid, references: :users, foreign_key: { to_table: :users }
-      t.belongs_to :account, type: :uuid, references: :account, foreign_key: { to_table: :accounts }
+      t.belongs_to :user, type: :uuid
+      t.belongs_to :account, type: :uuid
     end
   end
 end
