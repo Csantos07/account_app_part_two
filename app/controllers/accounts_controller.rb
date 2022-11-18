0d7bae978af2010b64defeea895e9b73f2e62151
Carlo Santos <rcarlosantos89@gmail.com>
2022-11-17 18:28:25 -0600

    only accounts of the current user, bootstrap, and seeds

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @accounts = Account.all
+    @accounts = current_user.accounts.all
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create at least one user
+user_one = User.create!(email: "antcs07@gmail.com", password: "password")
+user_two = User.create!(email: "antcs08@gmail.com", password: "password")
+
+
+# Account.create!(name: "first account", balance: 210000.00, user_id: user_two.id)
+# Account.create!(name: "second account", balance: 210000.00, user_id: user_two.id)
+
+account_one = Account.create!(name: "first account", balance: 210000.00)
+account_two = Account.create!(name: "second account", balance: 210000.00)
+
+user_one.accounts << [account_one, account_two]
