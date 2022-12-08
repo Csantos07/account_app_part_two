808efe829d9075caa8ac5185f15395b446e5313d
Carlo Santos <rcarlosantos89@gmail.com>
2022-12-08 17:23:53 -0600

    start crud for transactions no validations yet

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -14,9 +14,22 @@ class TransactionsController < ApplicationController
     redirect_to transactions_path
   end
 
+  def edit
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    Transaction.destroy(params[:id])
+
+    redirect_to transactions_path
+  end
+
   private
 
   def transaction_params
-    params.require(:transaction).permit(:name, :description, :balance, :user_id, :account_id)
+    params.require(:transaction).permit(:name, :date, :balance, :user_id, :account_id)
   end
 end
